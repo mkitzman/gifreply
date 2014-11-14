18c105bce69927728c97c12a63c93434ed70ca21
Michael Kitzman <dunkfudragon@yahoo.com>
2014-11-13 21:41:21 -0800

    code clean up

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,53 +1,69 @@
-/*global ZeroClipboard, console, document  */
-var client = new ZeroClipboard();
+/*global ZeroClipboard, console, document, $,   */
+/*jslint plusplus: true */
+
+
+var client, showImages;
+
+/* click to clip boared */
+client = new ZeroClipboard();
 client = new ZeroClipboard(document.getElementsByClassName("copy-button"));
 
-
-
-
-document.getElementById("reset-search").addEventListener("click", function clear() {
-
-    var els = document.querySelectorAll('.gif-container');
+//Function to find all hidden images and show them
+showImages = function() {
     
-    for (var i = 0; i < els.length; ++i) {
-        els[i].classList.remove('hidden');
+    'use strict';
+    var i,
+        hidden_photos = document.querySelectorAll('.image-container.hidden');
+
+    if(hidden_photos) {
+        for (i = 0; i < hidden_photos.length; ++i) {
+            hidden_photos[i].classList.remove('hidden');
+        }
     }
-    document.querySelector('.current-search').innerHTML = '';
-});
+};
 
-
-
-
+/* Auto Complete */
 $(function() {
-    var tags = document.getElementById("images").getAttribute('data-tag-list').split(" ");
 
-    console.log(tags);
-    var availableTags = tags;
+    'use strict';
+
+    var availableTags = document.getElementById("images-list").getAttribute('data-tag-list').split(" ");
     
     $( "#tags-search" ).autocomplete({
         source: availableTags,
         select: function(event, ui) {
-                    var hidden_els = document.querySelectorAll('.gif-container.hidden');
-                    if(hidden_els) {
-                        for (d = 0; d < hidden_els.length; ++d) {
-                            console.log(hidden_els[d]);
-                            hidden_els[d].classList.remove('hidden');
-                        }
-                    }
+            
+            
+            var tag,
+                els,
+                j;
 
-                    var tag = '.gif-container:not(.' + ui.item.value + ')';
-                    var els = document.querySelectorAll(tag);
+            //See if any elements have the hidden class, if so remove it
+            showImages();
 
-                    for (i = 0; i < els.length; ++i) {
-                        console.log(els[i]);
-                        els[i].classList.add('hidden');
-                    }
+            //create css selector based off of tag selected in autocomplete
+            //selector will be used to hide all photos without the search tab
+            tag = '.image-container:not(.' + ui.item.value + ')';
+            els = document.querySelectorAll(tag);
 
-                    document.querySelector('.current-search').innerHTML = ui.item.value;
+            for (j = 0; j < els.length; ++j) {
+                els[j].classList.add('hidden');
+            }
 
-                    
-
-                    $(this).val(''); return false;
-        },
+            //Display the tag searched
+            document.querySelector('.current-search').innerHTML = ui.item.value;
+            
+            //Clear search field so you can immediately type in more search terms
+            $(this).val(''); return false;
+        }
     });
+});
+
+/* Clear Hidden elements and tag searched text on clear button click */
+document.getElementById("reset-search").addEventListener("click", function clear() {
+
+    'use strict';
+
+    showImages();
+    document.querySelector('.current-search').innerHTML = '';
 });
